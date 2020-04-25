69757b12c8ed9a1df95a83641150dcd7d2d2f7b6
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2020-04-25 08:22:05 -0300

    Rodrigo Furlaneti

--- vendas-ng/src/app/comanda/component/comanda.component.ts
+++ vendas-ng/src/app/comanda/component/comanda.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-comanda',
   templateUrl: '../component/comanda.component.html',
-  styleUrls: ['../component/comanda.component.scss']
+  styleUrls: ['../component/comanda.component.scss'],
 })
 export class ComandaComponent implements OnInit {
   comandas: Comanda[];
@@ -21,16 +21,16 @@ export class ComandaComponent implements OnInit {
   //Selecionar todos os comandas
   public getComandas() {
     return this.comandaService.getComandas().subscribe(
-                  comandas => { this.comandas = comandas; }, 
-                  err => { console.log('Erro ao listar comandas', err);}
-                )
+      comandas => { this.comandas = comandas; }, 
+      err => { console.log('Erro ao listar comandas', err);}
+      )
   };
   //Selecionar todos os comandas
   public getClientes() {
     return this.clienteService.getClientes().subscribe(
-                  clientes => { this.clientes = clientes; }, 
-                  err => { console.log('Erro ao listar clientes', err);}
-                )
+      clientes => { this.clientes = clientes; }, 
+      err => { console.log('Erro ao listar clientes', err);}
+      )
   };
   //Deletar um comanda
   public deleteComanda(comanda: Comanda) {
@@ -40,6 +40,9 @@ export class ComandaComponent implements OnInit {
   }
   //Adicionar comanda
   public addComanda(form: NgForm) {
+    var str = this.comanda.data.toString();
+    let dt = this.fDate(str);
+    this.comanda.data = dt;
     if (this.comanda.id !== undefined) {
         this.comandaService.updateComanda(this.comanda).subscribe(() => {
           this.cleanForm(form);
@@ -60,5 +63,61 @@ export class ComandaComponent implements OnInit {
   //Editar comanda
   editComanda(comanda: Comanda) {
     this.comanda = { ...comanda };
+  }
+  public fDate(str){
+    var arr = str.split(" ");
+    console.log(arr);
+    arr.splice(0, 1);
+    arr.splice(3, 6);
+    if (arr[0] == "Jan"){
+      arr.splice(0, 1);
+      arr.push("Janeiro");
+    }
+    if (arr[0] == "Feb"){
+      arr.splice(0, 1);
+      arr.push("Fevereiro");
+    }
+    if (arr[0] == "Mar"){
+      arr.splice(0, 1);
+      arr.push("Março");
+    }
+    if (arr[0] == "Apr"){
+      arr.splice(0, 1);
+      arr.push("Abril");
+    }
+    if (arr[0] == "May"){
+      arr.splice(0, 1);
+      arr.push("Maio");
+    }
+    if (arr[0] == "Jun"){
+      arr.splice(0, 1);
+      arr.push("Junho");
+    }
+    if (arr[0] == "Jul"){
+      arr.splice(0, 1);
+      arr.push("Julho");
+    }
+    if (arr[0] == "Aug"){
+      arr.splice(0, 1);
+      arr.push("Agosto");
+    }
+    if (arr[0] == "Sep"){
+      arr.splice(0, 1);
+      arr.push("Setembro");
+    }
+    if (arr[0] == "Oct"){
+      arr.splice(0, 1);
+      arr.push("Outubro");
+    }
+    if (arr[0] == "Nov"){
+      arr.splice(0, 1);
+      arr.push("Novembro");
+    }
+    if (arr[0] == "Dec"){
+      arr.splice(0, 1);
+      arr.push("Dezembro");
+    }
+    let dt = arr[0]+"/"+arr[2]+"/"+arr[1];
+    return dt;
   }
 }
